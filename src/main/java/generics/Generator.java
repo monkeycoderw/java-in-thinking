1c3439b2975181506b05903fa4c96d835802b657
zhongwei <zhong_wei_cup@163.com>
2018-04-16 23:13:43 +0800

    接口泛型

---
+++ src/main/java/generics/Generator.java
@@ -0,0 +1,6 @@
+package generics;
+
+/**
+ * @author zhongwei Date: 2018/4/16 Time: 22:20.
+ */
+public interface Generator<T> { T next(); }
---
+++ src/main/java/generics/coffee/Americano.java
@@ -0,0 +1,7 @@
+package generics.coffee;
+
+/**
+ * @author zhongwei Date: 2018/4/16 Time: 22:34.
+ */
+public class Americano extends Coffee {
+}
