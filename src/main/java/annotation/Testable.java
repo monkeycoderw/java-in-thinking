1ae0b3476429aadea59c7afc8fbc827faa380927
victorWei19 <zhong_wei_cup@163.com>
2018-07-29 13:09:40 +0800

    annotation

---
+++ src/main/java/annotation/Testable.java
@@ -0,0 +1,11 @@
+package annotation;
+
+import net.mindview.atunit.Test;
+
+public class Testable {
+    public void execute() {
+        System.out.println("Executing..");
+    }
+
+    @Test void testExecute() { execute(); }
+}
---
+++ src/main/java/net/mindview/atunit/Test.java
@@ -0,0 +1,9 @@
+package net.mindview.atunit;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+public @interface Test {
+}
